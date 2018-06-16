ff322ee5444bca658b393f4ceb05dd7538ebad6d
George Ponick <gponick@gmail.com>
2018-06-16 17:29:02 -0400

    Dont be an idiot with an extra class. Being an idiot is why we need a safety net in the first place.

--- BTMLAISafetyNet/SafetyNet.cs
+++ BTMLAISafetyNet/SafetyNet.cs
@@ -17,49 +17,47 @@ using UnityEngine.UI;
 
 namespace BTMLAISafetyNet
 {
-    public class SafetyNet
+    public class Adapter<T>
     {
-        public class Adapter<T>
-        {
-            public readonly T instance;
-            public readonly Traverse traverse;
+        public readonly T instance;
+        public readonly Traverse traverse;
 
-            protected Adapter(T instance)
-            {
-                this.instance = instance;
-                traverse = Traverse.Create(instance);
-            }
-        }
-        [HarmonyPatch(typeof(AIUtil), "IsExposedToHostileFire")]
-        public static class AIUtil_IsExposedToHostileFire_Patch
+        protected Adapter(T instance)
         {
-            public static bool Prefix(AbstractActor unit, CombatGameState combat, out bool __result)
-            {
-                if (unit == null || combat == null)
-                {
-                    Logger.LogLine("isexposedtohostilefire: unit or  combat is null");
-                    __result = false;
-                    return false;
-                }
-                __result = false;
-                return true;
-            }
-        }
-
-        [HarmonyPatch(typeof(AIUtil), "IsExposedToHostileFireAndAlone")]
-        public static class AIUtil_IsExposedToHostileFireAndAlone_Patch
-        {
-            public static bool Prefix(AbstractActor unit, CombatGameState combat, out bool __result)
-            {
-                if (unit == null || combat == null)
-                {
-                    Logger.LogLine("isexposedtohostilefireandalone: unit or  combat is null");
-                    __result = false;
-                    return false;
-                }
-                __result = false;
-                return true;
-            }
+            this.instance = instance;
+            traverse = Traverse.Create(instance);
         }
     }
+    [HarmonyPatch(typeof(AIUtil), "IsExposedToHostileFire")]
+    public static class AIUtil_IsExposedToHostileFire_Patch
+    {
+        public static bool Prefix(AbstractActor unit, CombatGameState combat, out bool __result)
+        {
+            if (unit == null || combat == null)
+            {
+                Logger.LogLine("isexposedtohostilefire: unit or  combat is null");
+                __result = false;
+                return false;
+            }
+            __result = false;
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(AIUtil), "IsExposedToHostileFireAndAlone")]
+    public static class AIUtil_IsExposedToHostileFireAndAlone_Patch
+    {
+        public static bool Prefix(AbstractActor unit, CombatGameState combat, out bool __result)
+        {
+            if (unit == null || combat == null)
+            {
+                Logger.LogLine("isexposedtohostilefireandalone: unit or  combat is null");
+                __result = false;
+                return false;
+            }
+            __result = false;
+            return true;
+        }
+    }
+
 }
